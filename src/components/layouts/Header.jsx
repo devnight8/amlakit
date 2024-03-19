0d85656391c1a69bbe64103c69d904c65b81765c
devnight <siteteme@gamil.com>
2024-03-19 04:26:10 +0330

    add style in active menu

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,7 +1,11 @@
+"use client"
+
 import Link from "next/link";
 import MenuHumburger from "@/components/modules/MenuHumburger";
+import { usePathname } from "next/navigation";
 
 function Header() {
+const pathname = usePathname();
   return (
     <>
       <header className="px-2 md:px-0 flex justify-between items-center py-4 sticky z-10 top-0 bg-white ">
@@ -10,7 +14,7 @@ function Header() {
         </div>
         <div>
           <h3>
-            <Link className="font-bold text-2xl" href="/">
+            <Link className={`font-bold text-2xl`} href="/">
               املاکیت
             </Link>
           </h3>
@@ -18,17 +22,17 @@ function Header() {
         <div className="hidden md:block">
           <nav>
             <ul className="flex space-x-4  space-x-reverse group">
-              <li className="hover:text-primary transition-all">
-                <Link href="#">صفحه اصلی </Link>
+              <li className={pathname ==="/" ? ` text-primary` : `hover:text-primary transition-all`}>
+                <Link href="/">صفحه اصلی </Link>
               </li>
-              <li className="hover:text-primary transition-all">
-                <Link href="#">املاک</Link>
+              <li className={pathname ==="amlak" ? ` text-primary` : `hover:text-primary transition-all`}>
+                <Link href="/amlak">املاک</Link>
               </li>
-              <li className="hover:text-primary transition-all">
-                <Link href="#">درباره ما </Link>
+              <li className={pathname ==="about-us" ? ` text-primary` : `hover:text-primary transition-all`}>
+                <Link href="about-us">درباره ما </Link>
               </li>
-              <li className="hover:text-primary transition-all">
-                <Link href="#">تماس با ما </Link>
+              <li className={pathname ==="contact-us" ? ` text-primary` : `hover:text-primary transition-all`}>
+                <Link href="contact-us">تماس با ما </Link>
               </li>
             </ul>
           </nav>
